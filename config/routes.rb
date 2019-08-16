05ce3b46868329c9bb53ee454ce02ff72ecfd356
Miya Yuka <trrkn.l.s.s@gmail.com>
2019-08-16 13:32:22 +0900

    サンクスページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root "top#index"
+  #devise_for :users
+  #root "top#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+   get "sell/new" => "sell#new"
+   post "sell/create" => "sell#create"
+
+   get "/" => "top#index" #のちほど画面９に変更
 end
