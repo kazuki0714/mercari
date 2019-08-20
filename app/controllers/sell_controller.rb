9569a71d867201e3bf6e05eceeb1e1cadbf0ad1a
Miya Yuka <trrkn.l.s.s@gmail.com>
2019-08-20 13:39:26 +0900

    確認画面作成

--- app/controllers/sell_controller.rb
+++ app/controllers/sell_controller.rb
@@ -3,26 +3,27 @@ class SellController < ApplicationController
     @item = Item.new
   end
 
+  def confirm
+  @item = Item.new(title: params[:title],price: params[:price],stock: params[:stock],description: params[:description],image: "default_item.jpg")
+  render :new if @item.invalid?
+  end
+
   def create
     @item = Item.new(title: params[:title],price: params[:price],stock: params[:stock],description: params[:description],image: "default_item.jpg")
-
-        if params[:image]
-          @item.image = "#{@item.id}.jpg" #表示先のviewファイルへの記述　<img src="<%= "/item_images/#{@item.image}" %>">
-          image = params[:image]
-          File.binwrite("public/item_images/#{@item.image}", image.read)
-
-        end
-
-    if @item.save
+    #@item = Item.new(item_params)
+    if params[:image]
+      @item.image = "#{@item.id}.jpg" #表示先のviewファイルへの記述　<img src="<%= "/item_images/#{@item.image}" %>">
+      image = params[:image]
+      File.binwrite("public/item_images/#{@item.image}", image.read)
+    end
+    if params[:back]
+      format.html { render :new }
+    elsif @item.save
       flash[:notice] = "出品情報が保存されました"
       redirect_to("/sell/complete")
     else
       render("sell/new")
     end
-  end
-
-  def check
-    
   end
 
   def complete
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   root "top#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-   get "sell/complete"  => "sell#complete"
-   get "sell/check" => "sell#check"
-   get "sell/new" => "sell#new"
-   post "sell/create" => "sell#create"
+  get "sell/complete"  => "sell#complete"
+  post "sell/create" => "sell#create"
+  post "sell/confirm" => "sell#confirm"
+  get "sell/new" => "sell#new"
+
 
    get "/" => "top#index" #のちほど画面９に変更
 end
