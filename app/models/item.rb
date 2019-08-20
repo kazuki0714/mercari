9acbe6385566fa9dd21ab8678740f5c7e867cf19
Miya Yuka <trrkn.l.s.s@gmail.com>
2019-08-20 10:55:13 +0900

    確認画面作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,4 @@
 class Item < ApplicationRecord
   has_many :carts
-  belongs_to :user
+  belongs_to :user,optional: true #　optionalでbelongs_toのデフォルトが関連先の値を検査しないようにする
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   #devise_for :users
-  #root "top#index"
+  root "top#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+   get "sell/complete"  => "sell#complete"
+   get "sell/check" => "sell#check"
    get "sell/new" => "sell#new"
    post "sell/create" => "sell#create"
 
