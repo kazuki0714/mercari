28d30b4a89c153918f017aff16d6fe41bb15aed3
Miya Yuka <trrkn.l.s.s@gmail.com>
2019-08-30 15:07:09 +0900

    誤字修正

--- db/migrate/20190815082926_create_carts.rb
+++ db/migrate/20190815082926_create_carts.rb
@@ -3,8 +3,8 @@ class CreateCarts < ActiveRecord::Migration[5.2]
     create_table :carts do |t|
       #t.integer :quantity, null: false
       t.cart_item_id :string
-      t.references :items,　foreign_key: true
-      t.references :cart_items, foreign_key: true
+      t.references :item,　foreign_key: true
+      t.references :cart_item, foreign_key: true
       t.datetime :created_at
       t.datetime :updated_at
       t.timestamps
